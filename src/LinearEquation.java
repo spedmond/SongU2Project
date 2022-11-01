7d8cee2c51f15dfed6ffe48a4ab71c2ebe4e98bd
BT_3W2_03 <zoec58@nycstudents.net>
2022-11-01 11:56:04 -0400

    dadad

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,36 +10,55 @@ public class LinearEquation {
     private int y2;
     private String slope;
 
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
     public void getPoints() {
         System.out.println("Please enter your first point: ");
         p1 = input.nextLine();
         System.out.println("Please enter your second point: ");
         p2 = input.nextLine();
-    }
-    public String getSlope() {
         x1 = Integer.parseInt(p1.substring(1,2));
         x2 = Integer.parseInt(p2.substring(1,2));
         y1 = Integer.parseInt(p1.substring(4,5));
         y2 = Integer.parseInt(p2.substring(4,5));
+    }
+    public String getSlope() {
         int changeInX = x2-x1;
         int changeInY = y2-y1;
         slope = changeInY + "/" + changeInX;
 
-        if (changeInX==0) {
+        if (changeInX==0) { //undefined
             return "Undefined";
         }
-        else if (changeInX==1) {
+        else if (changeInY == 0) { //0 slope
+            return "0";
+        }
+        /*else if (changeInX == 1) { //pos whole #
             return "" + changeInY;
-        }
-        else if (changeInX==-1) {
+        } else if (changeInX == -1) { //neg whole #
             return "-" + changeInY;
+        } */
+        else if (changeInX == changeInY) { //slope of one
+            return "1";
         }
-        else if ((changeInX < 0 && changeInY > 0) || (changeInX > 0 && changeInY < 0)) {
+        else if (changeInX == changeInY * -1) { //slope of -1
+            return "-1";
+        }
+        else if (changeInX % 1.0==0.0 && changeInY % 1.0==0.0) {
+            return changeInY/changeInX + "";
+        }
+        else if ((changeInX < 0 && changeInY > 0) || (changeInX > 0 && changeInY < 0)) { //neg fraction slope
             return "-" + changeInY + "/" + changeInX;
         }
-        else {
+        else { //positive fraction slope
             return changeInY + "/" + changeInX;
         }
-
     }
+
+    public
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        LinearEquation equation = new LinearEquation();
+        LinearEquation equation = new LinearEquation(1,2,3,4);
 
         System.out.println("Welcome!");
         equation.getPoints();
         equation.getSlope();
-        equation.hi();
+        equation.();
     }
 }
